7de4b23bff6bd603e7ef676fa877e6ad9c918562
ajin5 <ajinmla94@gmail.com>
2024-09-03 15:06:54 +0530

    Dynamic Updated

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,11 +3,14 @@ import hero from '../assets/hero.png'
 import { AiOutlineTwitter, AiOutlineFacebook, AiOutlineLinkedin } from "react-icons/ai";
 
 export default function Hero() {
+  const exp = {
+    department:'Developing & Digital Marketing'
+  }
   return (
     <section id='/'className='flex flex-col md:flex-row bg-primary px-5 py-10'>
         <main className=' flex flex-col justify-center md:w-1/2 px-10 font-hero-font-name text-white text-2xl'>
             <h1>Hi, I am Ajin - Full Stack Developer & Digital Marketer</h1>
-            <p>I Expertise Developing & Digital Marketing</p>
+            <p>I expertise in {exp.department}</p>
             <div className='flex py-10'>
                 <a href='#' className='pr-5 hover:text-black'><AiOutlineLinkedin size={40} /></a>
                 <a href='#' className='pr-5 hover:text-black'><AiOutlineTwitter size={40}/></a>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,30 +4,40 @@ import blog from '../assets/blog.jpg'
 import food from '../assets/food.jpg'
 
 export default function Projects() {
+  const config ={
+    projects:[
+      {
+        image:ecommerce,
+        descrption:'This is an e-commerce website made using the MERN stack.',
+        link:''
+      },
+      {
+        image:blog,
+        descrption:'This is an blog website made using the MERN stack.',
+        link:''
+      },
+      {
+        image:food,
+        descrption:'This is an Food website made using the MERN stack.',
+        link:''
+      }
+    ]
+  }
   return (
     <section id='projects' className='flex flex-col px-10 py-10 bg-primary'>
         <div className=''>
             <h1 className='text-5xl text-white font-bold border-b-4 border-secondary w-[180px]'>Projects</h1>
         </div>
         <div className='w-full flex justify-between gap-5 pt-5'>
-            <div className='relative'>
-            <img className ='w-[300px] h-[200px]' src={ecommerce} alt='ecom'/>
-            <div className='project-desc'>
-            <p className='text-2xl text-white'>This is an e-commerce website made using the MERN stack.</p>
+          {config.projects.map((project)=>(<div className='relative'>
+            <img className ='w-[300px] h-[200px]' src={project.image} alt='ecom'/>
+            <div className='project-desc flex flex-col'>
+            <p className='text-2xl text-white  '>{project.descrption}</p>
+            <button className='flex justify-center pt-5 text-2xl text-white'>{project.link}Click here</button>
             </div>
-            </div>
-            <div className='relative'>
-            <img className ='w-[300px] h-[200px]' src={blog} alt='ecom'/>
-            <div className='project-desc'>
-            <p className='text-2xl text-white'>This is an Blog website made using the MERN stack.</p>
-            </div>
-            </div>
-            <div className='relative'>
-            <img className ='w-[300px] h-[200px]' src={food} alt='ecom'/>
-            <div className='project-desc'>
-            <p className='text-2xl text-white'>This is website made using the MERN stack.</p>
-            </div>
-            </div>
+            </div>))
+            
+}
         </div>
     </section>
   )
